c16b51ec32c490ea7448c22b9bd2ddf3b9921450
Henry Chan <79147973+henryphchan@users.noreply.github.com>
2025-12-22 03:23:30 -0800

    Update AdUnit.tsx

--- components/AdUnit.tsx
+++ components/AdUnit.tsx
@@ -17,21 +17,21 @@ export function AdUnit() {
     }, [pathname]);
 
     return (
-        <div className="w-full h-auto py-8 flex justify-center bg-slate-900/50 backdrop-blur-sm border-t border-white/5">
-            <div className="text-center">
-                <Script
-                    async
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3237862192285184"
-                    crossOrigin="anonymous"
-                    strategy="afterInteractive"
-                />
-                <ins className="adsbygoogle"
-                    key={pathname}
-                    style={{ display: 'block' }}
-                    data-ad-format="autorelaxed"
-                    data-ad-client="ca-pub-3237862192285184"
-                    data-ad-slot="8520077630"></ins>
-            </div>
+
+        <div className="text-center">
+            <Script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3237862192285184"
+                crossOrigin="anonymous"
+                strategy="afterInteractive"
+            />
+            <ins className="adsbygoogle"
+                key={pathname}
+                style={{ display: 'block' }}
+                data-ad-format="autorelaxed"
+                data-ad-client="ca-pub-3237862192285184"
+                data-ad-slot="8520077630"></ins>
         </div>
+
     );
 }
